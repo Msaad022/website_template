f284832848f7e40d5f9757ae5b8de7ef8befbe68
YaqeenAdel <helsadani01124@gmail.com>
2023-11-12 08:41:45 +0200

    tow commit

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -2,9 +2,9 @@ export const jqueryHandlerCustom = {
   function() {
     (function ($) {
       "use strict";
-      jQuery('.mean-menu').meanmenu({ 
-        meanScreenWidth: "991"
-      });
+      // jQuery('.mean-menu').meanmenu({ 
+      //   meanScreenWidth: "991"
+      // });
       // Header Sticky, Go To Top JS
       $(window).on("scroll", function () {
         // Header Sticky JS
@@ -41,6 +41,6 @@ export const jqueryHandlerCustom = {
           // Removes Active Class From Other Titles
           $(".accordion-title").not($(this)).removeClass("active");
         });
-    })(jQuery);
+    })();
   },
 };
